4f74b2c33acb750b6d08c9644b5bd9c418321673
jgonmor <jgonmor366@iessaladillo.es>
2025-07-29 18:47:50 +0200

    user status imp

--- programming-test/database/migrations/2025_07_28_171530_users.php
+++ programming-test/database/migrations/2025_07_28_171530_users.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignId('role_id')->references('id')->on('roles');
+            $table->string('status')->default('invitation sent');
             $table->rememberToken();
             $table->timestamps();
         });
