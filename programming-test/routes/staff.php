eeef42b49c5f2799a07c019ad53dcc59c3476f0b
jgonmor <jgonmor366@iessaladillo.es>
2025-08-07 11:45:58 +0200

    Fix: Staff can only access his own dashboard

--- programming-test/routes/staff.php
+++ programming-test/routes/staff.php
@@ -3,5 +3,8 @@
 use App\Http\Controllers\StaffController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('dashboard/{user_id}', [StaffController::class, 'index'])->name('staff');
+Route::get('dashboard/{user_id}', [StaffController::class, 'index'])
+    ->middleware('staff.dashboard')
+    ->name('staff');
+
 Route::get('policy/{id}', [StaffController::class, 'viewPolicy'])->name('staff.policy');
